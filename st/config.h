1bde2e8a88f80f2e7b706faa62e8b34101d22912
Roman Lopez Garro (d) <garrodev18@gmail.com>
2021-07-23 23:12:11 -0300

    besser

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack:style=Regular:pixelsize=15";
+static char *font = "Hack Nerd Font Mono:style=Regular:pixelsize=15";
 static int borderpx = 2;
 
 /*
