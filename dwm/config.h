fdafb0cf70f6a11f5c56a0fe8816771249e287fb
Roman Lopez Garro <46093870+rlgarro@users.noreply.github.com>
2022-06-05 14:31:03 -0300

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -2,18 +2,17 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack Nerd Font Mono:style=Bold:pixelsize=12",
-                                      };
-static const char dmenufont[]       = "Hack Nerd Font Mono:style=Bold:pixelsize=12";
-static const char col_gray1[]       = "#222222";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = {"Hack Nerd Font Mono:style=Bold:pixelsize=13"};
+static const char dmenufont[]       = "Hack Nerd Font Mono:style=Bold:pixelsize=13";
+static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#fbf1c7";
+static const char col_gray4[]       = "#000000";
+static const char col_cyan[]        = "#83a598";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -21,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "int", "pro", "stu", "med" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -68,23 +67,28 @@ static const Layout layouts[] = {
 	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
-#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/fish", "-c", cmd, NULL } }
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slock[]    = { "slock", NULL };
-static const char *firefox[]  = { "firefox", NULL };
+static const char *firefox[]  = { "chromium", NULL };
 static const char *pulsemixer[]  = { "st", "-e", "pulsemixer" };
-static const char *intellij[]  = { "idea.sh", NULL };
-static const char *bluectl[]  = { "st", "-e", "bluetoothctl" };
+//static const char *intellij[]  = { "idea.sh", NULL };
+//static const char *bluectl[]  = { "st", "-e", "bluetoothctl" };
+static const char *layout_change[]  = { "st", "-e", "layout_change" };
+static const char *de[]  = { "de", NULL };
+static const char *us[]  = { "us", NULL };
+static const char *es[]  = { "es", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_c,      spawn,          {.v = layout_change } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
@@ -107,11 +111,12 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefox} },
+	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefox } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slock } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = pulsemixer } },
-	{ MODKEY|ShiftMask,             XK_j,      spawn,          {.v = intellij } },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = bluectl } },
+	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = de } },
+	{ MODKEY|ShiftMask,             XK_n,      spawn,          {.v = es } },
+	{ MODKEY|ShiftMask,             XK_m,      spawn,          {.v = us } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
